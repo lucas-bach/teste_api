7f799b6a6e751e5ad3fa320d8c967d328cec7efd
Lucas Bach <lborgesbach@gmail.com>
2024-03-19 12:07:29 -0300

    get/vendas

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ vendas = {
     3:{"Item": "garrafa 750ml",'Preco-unitario': 10, "quantidade": 5},
     4:{"Item": "lata mini",'Preco-unitario': 2, "quantidade": 5},
 }
+
+@app.get("/")
+def home():
+    return {"Vendas": len(vendas)}
+
+
+@app.get("/vendas/{id_venda}")
+def pegar_venda(id_venda:int):
+    if id_venda in vendas:
+       return vendas[id_venda]
+    else:
+       return{"Erro": "ID Venda inesixtente"}
